979c36e5865405ea157b9eb9f0a763030dd085ed
cadenleekaizhen <cad3nlkz@gmail.com>
2025-10-21 08:29:51 +1030

    Update script.js

--- script.js
+++ script.js
@@ -69,3 +69,12 @@ form.addEventListener("submit", function (event) {
   }
 });
 
+// SW
+
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker.register("service-worker.js")
+        .then(() => console.log("Service Worker registered"))
+        .catch(err => console.log("SW registration failed:", err));
+    });
+  }
