99e1c7db516698dcb21b58bd06fefbe936f8b663
cadenleekaizhen <cad3nlkz@gmail.com>
2025-10-21 10:43:25 +1030

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "PWA-v2";
+const CACHE_NAME = "PWA-v3";
 const urlsToCache = [
   "./",
   "./index.html",
@@ -36,6 +36,7 @@ self.addEventListener("fetch", function(fetchEvent) {
       })
     );
 });
+
 
 
 
